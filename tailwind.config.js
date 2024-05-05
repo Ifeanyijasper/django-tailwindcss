715f250a4a3416f2899dca9f379002989f6e41b6
ifeanyijasper <ifeanyijasper11@gmail.com>
2024-05-06 00:24:19 +0100

    final editing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+// const colors = require('tailwindcss/colors')
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html, js}"],
   theme: {
+    // colors: {
+      // stone: colors.stone
+    // },
     extend: {},
   },
   plugins: [],
